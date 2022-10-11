6afff935ad35c05b4df32875ce4a35758be08e24
TreySchneider95 <tschneid95@gmail.com>
2022-10-11 11:34:17 -0500

    finished

--- main.js
+++ main.js
@@ -2,7 +2,31 @@
 // │ Your code here! │	
 // └─────────────────┘
 
+function isAdmin(user){
+    return user.userRole === "ADMIN" ? true : false
+}
 
+function getEmail(user){
+    return `${user.firstName.toLowerCase()}.${user.lastName.toLowerCase()}@codeimmersives.com`
+}
+
+function getPlaylistLength(playlist){
+    return playlist.songs.length
+}
+
+function getHardestHomework(homework){
+    let returnObj = homework[0]
+    for(x of homework){
+        if(returnObj.averageScore > x.averageScore){
+            returnObj = x
+        }
+    }
+    return homework.length > 0 ? returnObj.name : ''
+}
+
+function createPhonebook(names, phoneNumbers){
+    return names.reduce((obj, k, i) => ({...obj, [k]: phoneNumbers[i] }), {})
+}
 
 // ┌─────────────────────────────────────┐
 // │ Do not modify code below this line. │
